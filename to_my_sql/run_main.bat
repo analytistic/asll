a85b4a0fe4ae0afe095fa153200f45f2e35d8d7a
analytistic <1575670320@qq.com>
2024-08-13 11:17:23 +0800

    修改了一些环境配置

--- to_my_sql/run_main.bat
+++ to_my_sql/run_main.bat
@@ -5,6 +5,8 @@ cd /d D:\asl\to_my_sql
 REM 激活虚拟环境
 call D:\asl\venv\Scripts\activate.bat
 
+REM call D:\Anaconda3\Scripts\activate.bat you_env_name 如果你用anaconda管理环境 用这个命令
+
 REM 运行 main.py
 python main.py
 
